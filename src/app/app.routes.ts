ad60def0e3d2c053554a992a989472ae85719e30
Sylva Rakotovao <rakotovaosylva0@gmail.com>
2025-07-11 23:07:49 +0300

    [ADD] Repository ans useCase for user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { authGuard } from './infra/guard/auth.guard';
 import { StockComponent } from './presentation/pages/stock/stock.component';
 import { FournisseurComponent } from './presentation/pages/fournisseur/fournisseur.component';
 import { UtilisateurComponent } from './presentation/pages/utilisateur/utilisateur.component';
+import { CommandeComponent } from './presentation/pages/commande/commande.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -37,7 +38,7 @@ export const routes: Routes = [
         data: { roles: ['admin'] }
       },
       {
-        path: '', redirectTo: 'utilisateur', pathMatch: 'full' 
+        path: '', redirectTo: 'utilisateur', pathMatch: 'full'
       }
     ]
   },
